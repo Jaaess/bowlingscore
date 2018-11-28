2589ccca786f5c4121407c2df7340e7a2611e6f5
JawharMbarek <mbarek.jawhar@yahoo.de>
2018-11-28 23:55:56 +0100

    deleted unnötige Klassen

--- src/main/java/com/bowling/bowlingscore/App.java
+++ src/main/java/com/bowling/bowlingscore/App.java
@@ -1,28 +1,11 @@
 package com.bowling.bowlingscore;
 
-/**
- * Hello world!
- *
- */
+
+
 public class App {
-	
-	
+		
 	public static void main(String[] args) {
-		// System.out.println( "Hello World!" );
 		Game game = new Game();
-		game.addFrame(new Frame(1, 4));
-		System.out.println(game.actualbonus);
-		game.addFrame(new Frame(4, 5));
-		System.out.println(game.actualbonus);
-		game.addFrame(new Frame(6, 4)); 	// spare
-		System.out.println(game.actualbonus);
-		game.addFrame(new Frame(6, 4));
-		game.addFrame(new Frame(10, 0)); 	// strike
-		game.addFrame(new Frame(0, 1));
-		game.addFrame(new Frame(7, 3)); 	// spare
-		game.addFrame(new Frame(6, 4)); 	// spare
-		game.addFrame(new Frame(10, 0)); 	// strike
-		game.addFrame(new Frame(2, 8, 6)); 	// last frame with spare
-		System.out.println(game.score());
 	}
+	
 }
