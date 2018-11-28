40ca4a31b037434998db4bdd90a92d30bd23eb8f
JawharMbarek <mbarek.jawhar@yahoo.de>
2018-11-28 23:54:38 +0100

    completed Game Klasse

--- src/main/java/com/bowling/bowlingscore/Game.java
+++ src/main/java/com/bowling/bowlingscore/Game.java
@@ -7,12 +7,15 @@ public class Game {
 
 	// a bowling game is made of 10 frames
 	public List<Frame> frames = new ArrayList<Frame>();
-	private int bonus;
-	public int actualbonus = 0;
-	// number of the ball to throw: from 1 to 20
-	private int actualBall = 0;
+	// 10 scores for 10 frames
+	public List<Integer> scores = new ArrayList<Integer>();
+	
+	private int actualbonus = 0;
+	// number of the ball to throw from 0 to 20
+	private int thrownBalls = 0;
 
 	public Game() {
+		
 	}
 
 	public void addFrame(Frame frame) {
@@ -20,24 +23,64 @@ public class Game {
 	}
 
 	public void setBonus(int firstBall, int secondBall) {
-		if (frames.get(frames.size() - 1).isStrike())
+		if (frames.get(frames.size() - 1).isStrike()) {
 			actualbonus = firstBall + secondBall;
-		if (frames.get(frames.size() - 1).isSpare())
+			setActualbonus(actualbonus);
+		}
+		if (frames.get(frames.size() - 1).isSpare()) {
 			actualbonus = firstBall;
+			setActualbonus(actualbonus);
+		}
+	}
+
+	public boolean isThreeBallsRemaining() {
+		return true;
+	}
+
+	// save the calculated scores in list<Integer> for each frame
+	public void setScoreForFrame(int i) {
+		scores.add(i, score());
 	}
 
 	public int score() {
-		int score = 0;
-		for (int i = 0; i < frames.size(); i++) {
-			score += frames.get(i).score();
-		}
+
+		// get the last frame's score (calculated with old bonuses)
+		int score = scores.size() > 0 ? score = scores.get(scores.size() - 1) : 0;
+
+		// calculate the score with the actual bonus
+		score += frames.get(frames.size() - 1).score();
+
 		return score;
 	}
-
-	public boolean isNextFrameBonus() {
+	
+	public int scoreLastFrame(int firstBall, int secondBall){
+		if (frames.get(9).isStrike() == true || frames.get(9).isSpare() == true )
+			return 10;
+		else
+			return firstBall + secondBall;
+	}
+	
+	// check if there is a bonus 
+	public boolean isBonusToGetFromTheNextFrameExist() {
 		if (frames.get(frames.size() - 1).isStrike() || frames.get(frames.size() - 1).isSpare())
 			return true;
 		return false;
+	}
+
+	// return whether this is the last frame of the match
+	public boolean isLastFrame() {
+		if (frames.size() == 9)
+			return true;
+		return false;
+	}
+
+	public int getActualbonus() {
+		thrownBalls = 0;
+		return actualbonus;
+	}
+
+	public void setActualbonus(int actualbonus) {
+		this.actualbonus = actualbonus;
 	}
 
 }
