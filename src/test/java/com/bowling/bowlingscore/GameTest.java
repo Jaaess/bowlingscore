87703c00abf91a6e3a705b5ca1ee69de8e18285e
jbenhadj <jawhar.ben.hadj.m.barek@iav.de>
2018-11-29 15:24:53 +0100

    udjasted to 3 frames

--- src/test/java/com/bowling/bowlingscore/GameTest.java
+++ src/test/java/com/bowling/bowlingscore/GameTest.java
@@ -30,70 +30,70 @@ public class GameTest {
 		Assert.assertTrue("score ", game.score() == 14);
 		game.setScoreForFrame(1);
 
-		//frame 3
-		// spare
-		game.addFrame(new Frame(6, 4, game));
-		// check if the current frame spare or strike and get the bonus from the next frame
-		if (game.isBonusToGetFromTheNextFrameExist())
-			game.setBonus(5, 5);
-		Assert.assertTrue("score ", game.score() == 29);
-		game.setScoreForFrame(2);
+//		//frame 3
+//		// spare
+//		game.addFrame(new Frame(6, 4, game));
+//		// check if the current frame spare or strike and get the bonus from the next frame
+//		if (game.isBonusToGetFromTheNextFrameExist())
+//			game.setBonus(5, 5);
+//		Assert.assertTrue("score ", game.score() == 29);
+//		game.setScoreForFrame(2);
+//
+//		//frame 4
+//		// spare
+//		game.addFrame(new Frame(5, 5, game));
+//		if (game.isBonusToGetFromTheNextFrameExist())
+//			game.setBonus(10, 0);
+//		Assert.assertTrue("score ", game.score() == 49);
+//		game.setScoreForFrame(3);
+//
+//		//frame 5
+//		// strike
+//		game.addFrame(new Frame(10, 0, game));
+//		if (game.isBonusToGetFromTheNextFrameExist())
+//			game.setBonus(0, 1);
+//		Assert.assertTrue("score ", game.score() == 60);
+//		game.setScoreForFrame(4);
+//		
+//		//frame 6
+//		game.addFrame(new Frame(0, 1, game));
+//		if (game.isBonusToGetFromTheNextFrameExist())
+//			game.setBonus(7, 3);
+//		Assert.assertTrue("score ", game.score() == 61);
+//		game.setScoreForFrame(5);
+//
+//		//frame 7
+//		// spare
+//		game.addFrame(new Frame(7, 3, game));
+//		if (game.isBonusToGetFromTheNextFrameExist())
+//			game.setBonus(6, 4);
+//		Assert.assertTrue("score ", game.score() == 77);
+//		game.setScoreForFrame(6);
+//
+//		//frame 8
+//		// spare
+//		game.addFrame(new Frame(6, 4, game));
+//		if (game.isBonusToGetFromTheNextFrameExist())
+//			game.setBonus(10, 0);
+//		Assert.assertTrue("score ", game.score() == 97);
+//		game.setScoreForFrame(7);
+//
+//		//frame 9
+//		// strike
+//		game.addFrame(new Frame(10, 0, game));
+//		if (game.isBonusToGetFromTheNextFrameExist())
+//			game.setBonus(2, 8);
+//		Assert.assertTrue("score ", game.score() == 117);
+//		game.setScoreForFrame(8);
 
-		//frame 4
-		// spare
-		game.addFrame(new Frame(5, 5, game));
-		if (game.isBonusToGetFromTheNextFrameExist())
-			game.setBonus(10, 0);
-		Assert.assertTrue("score ", game.score() == 49);
-		game.setScoreForFrame(3);
-
-		//frame 5
-		// strike
-		game.addFrame(new Frame(10, 0, game));
-		if (game.isBonusToGetFromTheNextFrameExist())
-			game.setBonus(0, 1);
-		Assert.assertTrue("score ", game.score() == 60);
-		game.setScoreForFrame(4);
-		
-		//frame 6
-		game.addFrame(new Frame(0, 1, game));
-		if (game.isBonusToGetFromTheNextFrameExist())
-			game.setBonus(7, 3);
-		Assert.assertTrue("score ", game.score() == 61);
-		game.setScoreForFrame(5);
-
-		//frame 7
-		// spare
-		game.addFrame(new Frame(7, 3, game));
-		if (game.isBonusToGetFromTheNextFrameExist())
-			game.setBonus(6, 4);
-		Assert.assertTrue("score ", game.score() == 77);
-		game.setScoreForFrame(6);
-
-		//frame 8
-		// spare
-		game.addFrame(new Frame(6, 4, game));
-		if (game.isBonusToGetFromTheNextFrameExist())
-			game.setBonus(10, 0);
-		Assert.assertTrue("score ", game.score() == 97);
-		game.setScoreForFrame(7);
-
-		//frame 9
-		// strike
-		game.addFrame(new Frame(10, 0, game));
-		if (game.isBonusToGetFromTheNextFrameExist())
-			game.setBonus(2, 8);
-		Assert.assertTrue("score ", game.score() == 117);
-		game.setScoreForFrame(8);
-
-		//frame 10
+		// frame 3
 		// last frame
 		game.addFrame(new Frame(2, 8, game));
 		if (game.isLastFrame()) {
 			if (game.isThreeBallsRemaining()) {
-				Assert.assertTrue("score ", game.scores.get(8) + game.scoreLastFrame(2, 8) + /*third ball*/6  == 133);
+				Assert.assertTrue("score ", game.scores.get(1) + game.scoreLastFrame(2, 8) + /*third ball*/6  == 30);
 			}
-		}		
-		game.setScoreForFrame(9);
+		}
+//		game.setScoreForFrame(9);
 	}
 }
